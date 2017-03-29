340877db56ebf75a93fb9233d2eae6e8974bacdb
Andrew Rothstein <andrew.rothstein@gmail.com>
2017-03-28 22:11:04 -0400

    up to v0.0.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dcb',
-      version='0.0.6',
+      version='0.0.7',
       description='CLI for building Docker containers',
       url='http://github.com/andrewrothstein/dcb',
       author='Andrew Rothstein',
