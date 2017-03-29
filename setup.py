f532a2eeeb2d52dbfd8b6b7834e8b8f0326e7a73
Andrew Rothstein <andrew.rothstein@gmail.com>
2017-03-29 13:01:29 -0400

    snippets were not packaged correctly

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='dcb',
-      version='0.0.8',
+      version='0.0.9',
       description='CLI for building Docker containers',
       url='http://github.com/andrewrothstein/dcb',
       author='Andrew Rothstein',
       author_email='andrew.rothstein@gmail.com',
       license='MIT',
       packages=['dcb'],
+      package_data={"dcb": ["snippets/*"]},
       install_requires=['jinja2'],
       zip_safe=False,
       entry_points={
