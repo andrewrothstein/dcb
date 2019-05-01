20277187f15141a54d0fa5d81dbf0666931e9b54
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-04-30 22:13:07 -0400

    fix bug where failed builds were not failing the build process. configurate travis to push to pypi on tag drops.

--- src/dcb/image.py
+++ src/dcb/image.py
@@ -19,23 +19,23 @@ class Image:
         self.group = resolve_setting(group_settings)
         if not self.group:
             raise_missingarg(
-                "{0} group undefined".format(env_infix),
+                '{0} group undefined'.format(env_infix),
                 group_settings
             )
 
         self.app = resolve_setting(app_settings)
         if not self.app:
             raise_missingarg(
-                "{0} app undefined".format(env_infix),
+                '{0} app undefined'.format(env_infix),
                 app_settings
             )
-        self.tag = "latest" if tag is None else tag
+        self.tag = 'latest' if tag is None else tag
 
     def name(self):
-        return "{0}/{1}:{2}".format(self.group, self.app, self.tag)
+        return '{0}/{1}:{2}'.format(self.group, self.app, self.tag)
 
     def fq_name(self):
-        return "{0}/{1}".format(self.registry.registry, self.name())
+        return '{0}/{1}'.format(self.registry.registry, self.name())
 
 
 def group_setting(env_infix: str)-> List[Setting]:
@@ -67,7 +67,7 @@ def target_app_settings(env_infix)-> List[Setting]:
 
 
 def raise_missingarg(msg: str, settings: List[Setting]) -> None:
-    raise Exception("Missing Argument", msg + "; considered " + summarize_settings(settings))
+    raise Exception('Missing Argument', msg + '; considered ' + summarize_settings(settings))
 
 
 def upstream_image_builder(registry, group: str, app: str, tag: str) -> Image:
@@ -98,8 +98,8 @@ def write(
         snippet_loader: BaseLoader,
         snippets: List[str]
 ):
-    """writes ${OS}/Dockerfile and copies some stuff down..."""
-    log = logging.getLogger("dcb.write")
+    '''writes ${OS}/Dockerfile and copies some stuff down...'''
+    log = logging.getLogger('dcb.write')
     dbd = docker_build_dir(upstream_image.tag, sub_dirs)
     df = dockerfile(upstream_image.tag, sub_dirs)
 
@@ -110,15 +110,15 @@ def write(
             copy_file(dbd, f)
 
     templates = resolve_templates(snippets, snippet_loader)
-    log.info("processing {0} templates to Dockerfile to {0}...".format(len(templates), df))
+    log.info('processing {0} templates to Dockerfile to {0}...'.format(len(templates), df))
     with open(df, 'w') as f:
         for t in templates:
             f.write(
                 t.render({
-                    "fq_upstream_image": upstream_image.fq_name()
+                    'fq_upstream_image': upstream_image.fq_name()
                 })
             )
-            f.write("\n")
+            f.write('\n')
 
 
 def describe(image: Image) -> int:
@@ -126,32 +126,41 @@ def describe(image: Image) -> int:
 
 
 def build(target_image: Image, build_envs: List[str], sub_dirs: bool) -> int:
-    log = logging.getLogger("dcb.build")
-    log.info("building the {0} container...".format(target_image.name()))
+    log = logging.getLogger('dcb.build')
+    log.info('building the {0} container...'.format(target_image.name()))
     cmd = ['docker', 'build', '--rm=false']
     cmd += fmt_build_args(build_envs)
     cmd += ['-t', target_image.name()]
     cmd += ['-f', dockerfile(target_image.tag, sub_dirs)]
     cmd += [docker_build_dir(target_image.tag, sub_dirs)]
     rc = run_it(cmd)
-    describe(target_image)
-    return rc
+    if rc == 0:
+        describe(target_image)
+    else:
+        raise Exception('Error building {0}: {1}'.format(target_image, rc))
 
 
 def push(target_image: Image) -> int:
-    log = logging.getLogger("dcb.push")
-    log.info("tagging {0} as {1}...".format(target_image.name(), target_image.fq_name()))
-    run_it(['docker', 'tag', target_image.name(), target_image.fq_name()])
-    log.info("pushing {0}...".format(target_image.fq_name()))
-    r = run_it(['docker', 'push', target_image.fq_name()])
-    describe(target_image)
-    return r
+    log = logging.getLogger('dcb.push')
+    log.info('tagging {0} as {1}...'.format(target_image.name(), target_image.fq_name()))
+    rc = run_it(['docker', 'tag', target_image.name(), target_image.fq_name()])
+    if rc == 0:
+        log.info('pushing {0}...'.format(target_image.fq_name()))
+        rc = run_it(['docker', 'push', target_image.fq_name()])
+        if rc == 0:
+            describe(target_image)
+        else:
+            raise Exception('Error pushing {0}: {1}'.format(target_image.fq_name(), rc))
+    else:
+        raise Exception('Error tagging {0} as {1}...'.format(target_image.name(), target_image.fq_name()))
 
 
 def pull(upstream_image: Image) -> int:
-    log = logging.getLogger("dcb.pull")
-    log.info("pulling {0}...".format(upstream_image.fq_name()))
-    r = run_it(['docker', 'pull', upstream_image.fq_name()])
-    describe(upstream_image)
-    return r
-
+    log = logging.getLogger('dcb.pull')
+    upstream_image_fq_name = upstream_image.fq_name()
+    log.info('pulling {0}...'.format(upstream_image_fq_name))
+    rc = run_it(['docker', 'pull', upstream_image_fq_name])
+    if rc == 0:
+        describe(upstream_image)
+    else:
+        raise Exception('Error pulling {0}: {1}'.format(upstream_image_fq_name, rc))
