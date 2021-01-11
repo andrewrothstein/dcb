0a40d6370d8debd0cc53fd8d61cc35a3c86b224d
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-01-11 10:22:12 -0500

    staging v0.0.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dcb',
-      version='0.0.17',
+      version='0.0.18',
       description='CLI for building, tagging, and publishing Docker containers',
       url='http://github.com/andrewrothstein/dcb',
       author='Andrew Rothstein',
