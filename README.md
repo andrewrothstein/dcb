91be21daeb22d2daff8df4c75af7366f5fd869a7
Andrew Rothstein <andrewrothstein@users.noreply.github.com>
2023-03-12 14:35:43 -0400

    task based cicd (#6)
    * integrate a task based ci/cd
    
    * fix readme and setup, stage 0.1.3

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 dcb: A Docker container image family building CLI
-=================================================
-
-# Upstream Image Family
-* upstream-registry.com/upstream-groupname/upstream-appname:platform
-
-# Target Image 
-* target-registry.com/target-groupname/target-appname:platform
--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='dcb',
-      version='0.1.2',
+      version='0.1.3',
       description='CLI for building, tagging, and publishing Docker containers',
       url='http://github.com/andrewrothstein/dcb',
       author='Andrew Rothstein',
@@ -15,6 +18,8 @@ setup(name='dcb',
       extras_require={
           "test": ['nose2']
       },
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       zip_safe=False,
       entry_points={
           'console_scripts': [
